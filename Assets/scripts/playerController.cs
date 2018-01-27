b5e0ce97f6d5734c3c3b9e86d5ee2ada9bfcbf1a
Kshitij Khode <kshitij.khode@gmail.com>
2018-01-26 21:12:11 -0500

    [SCRIPTS,ASSETS] Add kRollABall unity example

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour {
+    private Rigidbody rb;
+    private int count;
+
+    public float moveSpeed;
+    public Text countText;
+    public Text winText;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate ()
+    {
+        rb.AddForce(moveSpeed*(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"))));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count" + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
